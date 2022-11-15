2087718816566c2bb0873742ca044e2304701b00
Roger Trujillo <rtrujillo@unal.edu.co>
2022-11-14 22:40:24 -0500

    remove unused files

--- refinement.cpp
+++
@@ -1,8 +0,0 @@
-Mesh mesh(mesh_file, 1, 1);
-int dim = mesh.Dimension();
-
-int ref_levels = (int)floor(log(50000./mesh.GetNE())/log(2.)/dim);
-for (int l = 0; l < ref_levels; l++)
-{
-   mesh.UniformRefinement();
-}
